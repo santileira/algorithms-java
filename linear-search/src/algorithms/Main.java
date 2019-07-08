ef3418ef7542bfab4d071f346135afb81a49d4ad
santileira <santiago.leira@mercadolibre.co>
2019-07-08 20:39:25 -0300

    add linear-search

---
+++ linear-search/src/algorithms/Main.java
@@ -0,0 +1,32 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int sizeOfArray = input.nextInt();
+        int elementSearched = input.nextInt();
+
+        int result = -1;
+
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        int element;
+
+        for (int i = 0; i < sizeOfArray; i++) {
+            element = input.nextInt();
+            array.add(element);
+        }
+
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i) == elementSearched) {
+                result = i + 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
